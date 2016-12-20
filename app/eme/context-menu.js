3207ba3d1d7b64c227a057982e5483a43665c0e0
Kristian Rekstad <krirek@msn.com>
2016-12-20 21:34:55 +0100

    add link to markdown help in context menu

--- app/eme/context-menu.js
+++ app/eme/context-menu.js
@@ -1,12 +1,14 @@
 'use strict'
 const {
+  BrowserWindow,
   MenuItem,
   shell
 } = require('electron')
 
 module.exports = {
-  append(params) {
+  append(params, browserWindow) {
     const text = params.selectionText
+    
     return [
       new MenuItem({
         type: 'separator'
@@ -18,6 +20,17 @@ module.exports = {
           if (text) {
             shell.openExternal(`https://www.google.com/search?q=${text}`)
           }
+        }
+      }),
+      new MenuItem({
+        label: 'Show Markdown help',
+        click() {
+          let child = new BrowserWindow({parent: browserWindow, modal: true, useContentSize: true})
+          child.on('closed', () => {
+            child = null
+          })
+          child.setMenu(null)
+          child.loadURL('https://help.github.com/articles/basic-writing-and-formatting-syntax/')
         }
       })
     ]
